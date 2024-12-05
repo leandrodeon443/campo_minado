74665f6812727fcd77d94682bc68e3e0b25877ed
teste <teste@LDEON>
2024-12-05 20:45:46 -0300

    Add project files.

---
+++ campo_minado/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace campo_minado
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ campo_minado/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace campo_minado
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_NovoJogo(object sender, EventArgs e)
+        {
+            DisplayAlert("Novo Jogo", "Um novo jogo foi iniciado", "ok");
+            return;
+        }
+    }
+
+}
