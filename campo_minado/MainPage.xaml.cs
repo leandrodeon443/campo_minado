78c40228ef376224a75df692a51a192bc1b72fa8
Leandro Deon <leandrodeon443@gmail.com>
2024-12-05 21:03:18 -0300

    Testando Linter Commit no git actions

--- campo_minado/MainPage.xaml.cs
+++ campo_minado/MainPage.xaml.cs
@@ -1,19 +1,20 @@
-﻿namespace campo_minado
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+﻿namespace campo_minado {
+    public partial class MainPage: ContentPage {
+        public MainPage() 
+            
+            {
             InitializeComponent();
         }
 
-        private void BTN_NovoJogo(object sender, EventArgs e)
-        {
+        private void BTN_NovoJogo(object sender, EventArgs e) 
+            
+            {
             DisplayAlert("Novo Jogo", "Um novo jogo foi iniciado", "ok");
             return;
         }
 
         private void BTN_Salvos(object sender, EventArgs e) 
+            
             {
             DisplayAlert("Jogos Salvos", "Mostrando jogos salvos", "ok");
             return;
